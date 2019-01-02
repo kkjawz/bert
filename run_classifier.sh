75e1b7ee3c84e76713902191d837226fccf9a6b4
benkantor <benkantor@mail.huji.ac.il>
2019-01-02 12:57:49 +0200

    run_classifier.sh

---
+++ run_classifier.sh
@@ -0,0 +1,17 @@
+export BERT_BASE_DIR=/specific/netapp5_2/gamir/benkantor/models/bert/uncased_L-12_H-768_A-12
+#export BERT_BASE_DIR=/specific/netapp5_2/gamir/benkantor/models/bert/uncased_L-24_H-1024_A-16
+export GLUE_DIR=/specific/netapp5_2/gamir/benkantor/data/glue_data
+
+python run_classifier.py \
+  --task_name=MRPC \
+  --do_train=true \
+  --do_eval=true \
+  --data_dir=$GLUE_DIR/MRPC \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+  --max_seq_length=128 \
+  --train_batch_size=6 \
+  --learning_rate=2e-5 \
+  --num_train_epochs=3.0 \
+  --output_dir=/tmp/mrpc_output/
